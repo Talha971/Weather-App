b44e5bb0ad329a006373b603393cfa23596e84d7
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2024-02-08 00:49:15 +0500

    updated

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 document.getElementById("btn").addEventListener("click", () => {
-  let input = document.getElementById("inp").value;
+  let input = document.getElementById("inp");
 
   fetch(
-    `https://api.weatherapi.com/v1/current.json?key=fa7e6e9d66b843a3ab8125723242901&q=${input}&aqi=no`
+    `https://api.weatherapi.com/v1/current.json?key=fa7e6e9d66b843a3ab8125723242901&q=${input.value}&aqi=no`
   )
     .then((response) => response.json())
     .then((data) => {
+      input.value = "";
       const box = document.getElementById("box");
       box.innerHTML = `<br>
         <p>${data.current.condition.text}</p>
@@ -13,11 +14,11 @@ document.getElementById("btn").addEventListener("click", () => {
         <p id="temp"> ${data.current.temp_c} °C</p>
         <p id="loc">${data.location.name},  ${data.location.country}</p>
         <p>Wind: ${data.current.wind_kph} km/h</p>
-                `;
+        `;
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
       const box = document.getElementById("box");
-      box.innerHTML = "<p>Weather information not available.</p>";
+      box.innerHTML = "<br><p>Weather information not available.</p>";
     });
 });
